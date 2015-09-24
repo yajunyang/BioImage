7705f46b98050f7bb9c74bfbe76f87177cbd53fc
yajunyang <chinayangyajun@gmail.com>
2015-09-24 12:44:59 +0800

    Add some local threshold segmentation methods!

---
+++ src/yang/BackGroundChange.java
@@ -0,0 +1,19 @@
+package yang;
+
+import ij.ImagePlus;
+import ij.WindowManager;
+import ij.plugin.PlugIn;
+
+public class BackGroundChange implements PlugIn{
+
+	private ImagePlus ips;
+	
+	@Override
+	public void run(String arg) {
+		ips = WindowManager.getCurrentImage();
+		if(null == ips) 
+			return;
+		
+	}
+
+}
--- src/yang/Main.java
+++ src/yang/Main.java
@@ -5,5 +5,6 @@ import ij.ImageJ;
 public class Main {
 	public static void main(String[] args) {
 		ImageJ.main(args);
-	}	
+	}
 }
+
